a955f9f21a549287eaefb2220103b43e8bc79096
yasser <meryasser167@gmail.com>
2026-03-14 04:41:16 -0700

    Counting sheep

---
+++ Counting sheep.js
@@ -0,0 +1,36 @@
+let array = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+let result = function () {
+  let counter = 0;
+  array.map(function (ele) {
+    if (ele === true) {
+      counter++;
+    }
+  });
+  return counter;
+};
+console.log(result());
