b1cd16108e7f2208ca6cac3500459cef662531fd
mohamed <mryasser167@gmail.com>
2026-03-28 13:46:38 -0700

    ###

---
+++ Remove String Spaces.js
@@ -0,0 +1,7 @@
+function ya(x) {
+  return x
+    .trim()
+    .split("")
+    .filter((ele) => ele != " ");
+}
+console.log(ya(" y a s s e r "));
---
+++ Reversingstring.js
@@ -0,0 +1,4 @@
+function mo(string) {
+  return string.split(" ").reverse().join(" ");
+}
+console.log(mo("mohamed yasser."));
