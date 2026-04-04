52c5cc1e26e5442e958026d6021c650ea5550792
mohamed <mryasser167@gmail.com>
2026-04-04 12:54:53 -0700

    switch

---
+++ switch.js
@@ -0,0 +1,25 @@
+function ya(num) {
+  switch (num) {
+    case 1:
+      return `one`;
+      break;
+    case 2:
+      return `two`;
+      break;
+    case 3:
+      return `three`;
+      break;
+    case 4:
+      return `four`;
+      break;
+    case 5:
+      return `five`;
+      break;
+    case 6:
+      return `six`;
+      break;
+    default:
+      return `wrong`;
+  }
+}
+console.log(ya(7));
