93a0f111b030d01a5f534d080463390b7ffa137c
mohamed <mryasser167@gmail.com>
2026-04-04 12:25:54 -0700

    welcome

---
+++ Welcome.js
@@ -0,0 +1,23 @@
+function ya(lang) {
+  let languages = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  return languages[lang] || languages["english"];
+}
+console.log(ya("finnis"));
